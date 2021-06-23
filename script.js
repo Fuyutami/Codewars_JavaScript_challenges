5df8365ed3666a019beeafbdb1cf6c0c8f3cbe73
[Fuyutami] <fuyutami.art@gmail.com>
2021-06-23 14:11:35 +0300

    add 'Josephus Permutation' challenge (5-kyu)

--- script.js
+++ script.js
@@ -443,49 +443,97 @@
 // All the test strings would contain valid Morse code, so you may skip checking for errors and exceptions. In C#, tests will fail if the solution code throws an exception, please keep that in mind. This is mostly because otherwise the engine would simply ignore the tests, resulting in a "valid" solution.
 
 
-const MORSE_CODE = {
-  '.-':     'a',
-  '-...':   'b',
-  '-.-.':   'c',
-  '-..':    'd',
-  '.':      'e',
-  '..-.':   'f',
-  '--.':    'g',
-  '....':   'h',
-  '..':     'i',
-  '.---':   'j',
-  '-.-':    'k',
-  '.-..':   'l',
-  '--':     'm',
-  '-.':     'n',
-  '---':    'o',
-  '.--.':   'p',
-  '--.-':   'q',
-  '.-.':    'r',
-  '...':    's',
-  '-':      't',
-  '..-':    'u',
-  '...-':   'v',
-  '.--':    'w',
-  '-..-':   'x',
-  '-.--':   'y',
-  '--..':   'z',
-  '.----':  '1',
-  '..---':  '2',
-  '...--':  '3',
-  '....-':  '4',
-  '.....':  '5',
-  '-....':  '6',
-  '--...':  '7',
-  '---..':  '8',
-  '----.':  '9',
-  '-----':  '0',
+// const MORSE_CODE = {
+//   '.-':     'a',
+//   '-...':   'b',
+//   '-.-.':   'c',
+//   '-..':    'd',
+//   '.':      'e',
+//   '..-.':   'f',
+//   '--.':    'g',
+//   '....':   'h',
+//   '..':     'i',
+//   '.---':   'j',
+//   '-.-':    'k',
+//   '.-..':   'l',
+//   '--':     'm',
+//   '-.':     'n',
+//   '---':    'o',
+//   '.--.':   'p',
+//   '--.-':   'q',
+//   '.-.':    'r',
+//   '...':    's',
+//   '-':      't',
+//   '..-':    'u',
+//   '...-':   'v',
+//   '.--':    'w',
+//   '-..-':   'x',
+//   '-.--':   'y',
+//   '--..':   'z',
+//   '.----':  '1',
+//   '..---':  '2',
+//   '...--':  '3',
+//   '....-':  '4',
+//   '.....':  '5',
+//   '-....':  '6',
+//   '--...':  '7',
+//   '---..':  '8',
+//   '----.':  '9',
+//   '-----':  '0',
+// }
+
+// decodeMorse = function(morseCode){ 
+//   return morseCode.trim().split('   ').map(x => x.split(' ').map(x => MORSE_CODE[x]).join('')).join(' ')
+// }
+
+// console.log(decodeMorse('.... . -.--   .--- ..- -.. .'))
+
+//***********************************************************************************************************************************
+
+
+// Josephus Permutation
+
+// This problem takes its name by arguably the most important event in the life of the ancient historian Josephus: according to his tale, he and his 40 soldiers were trapped in a cave by the Romans during a siege.
+
+// Refusing to surrender to the enemy, they instead opted for mass suicide, with a twist: they formed a circle and proceeded to kill one man every three, until one last man was left (and that it was supposed to kill himself to end the act).
+
+// Well, Josephus and another man were the last two and, as we now know every detail of the story, you may have correctly guessed that they didn't exactly follow through the original idea.
+
+// You are now to create a function that returns a Josephus permutation, taking as parameters the initial array/list of items to be permuted as if they were in a circle and counted out every k places until none remained.
+
+// Tips and notes: it helps to start counting from 1 up to n, instead of the usual range 0..n-1; k will always be >=1.
+
+// For example, with n=7 and k=3 josephus(7,3) should act this way.
+
+// [1,2,3,4,5,6,7] - initial sequence
+// [1,2,4,5,6,7] => 3 is counted out and goes into the result [3]
+// [1,2,4,5,7] => 6 is counted out and goes into the result [3,6]
+// [1,4,5,7] => 2 is counted out and goes into the result [3,6,2]
+// [1,4,5] => 7 is counted out and goes into the result [3,6,2,7]
+// [1,4] => 5 is counted out and goes into the result [3,6,2,7,5]
+// [4] => 1 is counted out and goes into the result [3,6,2,7,5,1]
+// [] => 4 is counted out and goes into the result [3,6,2,7,5,1,4]
+// So our final result is:
+
+// josephus([1,2,3,4,5,6,7],3)==[3,6,2,7,5,1,4]
+
+function josephus(items,k){
+  let order = []
+  let position = 0
+
+  while (items.length > 0) {    
+      for (let i = 1; i <= k; i++) {
+          position++
+          if (position > items.length)  position = 1   
+      }
+      console.log(position);
+      order.push(items[position - 1])
+      items.splice(position - 1, 1)
+      position--
+  }
+  return order
 }
 
-decodeMorse = function(morseCode){ 
-  return morseCode.trim().split('   ').map(x => x.split(' ').map(x => MORSE_CODE[x]).join('')).join(' ')
-}
-
-console.log(decodeMorse('.... . -.--   .--- ..- -.. .'))
+console.log(josephus([1,2,3,4,5,6,7],3))
 
 //***********************************************************************************************************************************
